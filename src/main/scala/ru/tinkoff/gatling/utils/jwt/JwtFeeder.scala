cf01606242d9e2bc5ee609a5a560aeb07faa44e1
chepk <chepk@yandex.ru>
2020-10-05 09:53:03 +0300

    update jwtFeeder args

--- src/main/scala/ru/tinkoff/gatling/utils/jwt/JwtFeeder.scala
+++ src/main/scala/ru/tinkoff/gatling/utils/jwt/JwtFeeder.scala
@@ -4,33 +4,27 @@ import io.gatling.core.Predef._
 import io.gatling.core.structure.{ChainBuilder, ScenarioBuilder}
 
 object JwtFeeder {
-  def apply(headerPath: String, payloadPath: String, tokenName: String, secretToken: String, jwtAlgorithm: String): JwtFeeder =
-    new JwtFeeder(headerPath, payloadPath, tokenName, secretToken, jwtAlgorithm)
+  def apply(headerPath: String, payloadPath: String): JwtFeeder =
+    new JwtFeeder(headerPath, payloadPath)
 }
 
-class JwtFeeder(headerPath: String, payloadPath: String, tokenName: String, secretToken: String, jwtAlgorithm: String) {
+class JwtFeeder(headerPath: String, payloadPath: String) {
 
   val generator = JwtGenerator(headerPath, payloadPath)
 
   implicit class ScenarioAppender(build: ScenarioBuilder)(implicit s: Session) {
-    def setJwt(tokenName: String,
-               secretToken: String,
-               jwtAlgorithm: String): ScenarioBuilder = {
+    def setJwt(tokenName: String, secretToken: String, jwtAlgorithm: String): ScenarioBuilder =
       generator
         .generateJwt(secretToken, jwtAlgorithm)
         .map(jwt => build.exec(_.set(tokenName, jwt)))
         .getOrElse(build)
-    }
   }
 
   implicit class ChainAppender(build: ChainBuilder)(implicit s: Session) {
-    def setJwt(tokenName: String,
-               secretToken: String,
-               jwtAlgorithm: String): ChainBuilder = {
+    def setJwt(tokenName: String, secretToken: String, jwtAlgorithm: String): ChainBuilder =
       generator
         .generateJwt(secretToken, jwtAlgorithm)
         .map(jwt => build.exec(_.set(tokenName, jwt)))
         .getOrElse(build)
-    }
   }
 }
