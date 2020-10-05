eff0c950ad02ad839cfe212661612c9295067c16
chepk <chepk@yandex.ru>
2020-10-05 15:38:13 +0300

    add jwt usage example

--- example/src/test/scala/ru/tinkoff/load/example/scenarios/SampleScenario.scala
+++ example/src/test/scala/ru/tinkoff/load/example/scenarios/SampleScenario.scala
@@ -5,6 +5,8 @@ import io.gatling.core.structure.ScenarioBuilder
 import io.gatling.http.Predef._
 import io.gatling.http.request.builder.HttpRequestBuilder
 import ru.tinkoff.load.example.feeders.Feeders._
+import ru.tinkoff.gatling.utils.jwt._
+import ru.tinkoff.gatling.config.SimulationConfig._
 
 object SampleScenario {
   def apply(): ScenarioBuilder = new SampleScenario().sampleScenario
@@ -12,12 +14,19 @@ object SampleScenario {
 
 class SampleScenario {
 
+  //load JWT templates in memory
+  val jwtGenerator = JwtGenerator("jwtTemplates/header.json", "jwtTemplates/payload.json")
+  //get sensitive data from ENV or JAVA_OPTS
+  val secretToken  = getStringParam("secretToken")
+
+  //single request
   val getMainPage: HttpRequestBuilder = http("GET /")
     .get("/")
     .check(status is 200)
 
-  //include feeders in scenario as usual
+  //compose all in scenario
   val sampleScenario: ScenarioBuilder = scenario("Sample scenario")
+  //include feeders in scenario as usual
     .feed(firstWorkDayHours)
     .feed(holidaysDate)
     .feed(myCustomFeeder)
@@ -34,6 +43,11 @@ class SampleScenario {
     .feed(list2feeder)
     .feed(finiteRandomDigitsWithTransform)
     .feed(regexString)
+    //generate JWT using feeders current data
+    .exec(_.setJwt(jwtGenerator, "myToken", secretToken, "HS256"))
+    //set JWT cookie
+    .exec(addCookie(Cookie("JWT_TOKEN", "${myToken}", Option(getStringParam("domain")), Option("/"))))
+    //execute request
     .exec(getMainPage)
 
 }
