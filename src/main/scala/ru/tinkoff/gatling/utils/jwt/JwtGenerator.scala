e28253b026c96072ce69255e0c383a9dfdcd662a
chepk <chepk@yandex.ru>
2020-10-06 10:49:12 +0300

    hide private jwt generator methods

--- src/main/scala/ru/tinkoff/gatling/utils/jwt/JwtGenerator.scala
+++ src/main/scala/ru/tinkoff/gatling/utils/jwt/JwtGenerator.scala
@@ -13,18 +13,18 @@ object JwtGenerator {
 
 private[jwt] class JwtGenerator(headerPath: String, payloadPath: String) {
 
-  def readResource(path: String): Option[String] = Option(Source.fromResource(path).mkString)
+  private def readResource(path: String): Option[String] = Option(Source.fromResource(path).mkString)
 
-  val header: Option[String]  = readResource(headerPath)
-  val payload: Option[String] = readResource(payloadPath)
+  private val header: Option[String]  = readResource(headerPath)
+  private val payload: Option[String] = readResource(payloadPath)
 
-  def compileResource(json: String)(implicit s: Session): Option[String] =
+  private def compileResource(json: String)(implicit s: Session): Option[String] =
     ElCompiler.compile[String](json).apply(s).toOption
 
-  def jwtAlgorithmFromString(jwtAlgorithm: String): Option[JwtAlgorithm] =
+  private def jwtAlgorithmFromString(jwtAlgorithm: String): Option[JwtAlgorithm] =
     JwtAlgorithm.optionFromString(jwtAlgorithm.toUpperCase)
 
-  def encodeJwt(header: String, payload: String, secretToken: String, jwtAlgorithm: JwtAlgorithm): String = {
+  private def encodeJwt(header: String, payload: String, secretToken: String, jwtAlgorithm: JwtAlgorithm): String = {
     Jwt.encode(header, payload, secretToken, jwtAlgorithm)
   }
 
