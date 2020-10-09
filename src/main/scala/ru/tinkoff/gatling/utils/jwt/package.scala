55ba49cc1e57db411389ebe98b6c756a814b4193
chepk <chepk@yandex.ru>
2020-10-09 10:32:53 +0300

    refactor jwt generator

--- src/main/scala/ru/tinkoff/gatling/utils/jwt/package.scala
+++ src/main/scala/ru/tinkoff/gatling/utils/jwt/package.scala
@@ -1,25 +1,25 @@
 package ru.tinkoff.gatling.utils
 
+import com.typesafe.scalalogging.StrictLogging
+import io.gatling.commons.validation.{Failure, Success}
 import io.gatling.core.Predef.Session
-import io.gatling.core.session.el.ElCompiler
 import pdi.jwt.Jwt
+import io.gatling.core.session.el._
 
-package object jwt {
+package object jwt extends StrictLogging {
   def jwt(algorithm: String, secret: String): JwtGeneratorBuilder = {
-    JwtGeneratorBuilder(Header(algorithm), Payload(), algorithm, secret)
+    JwtGeneratorBuilder(Header(), Payload(), algorithm, secret)
   }
 
   implicit class SessionAppender(s: Session) {
     def setJwt(generator: JwtGeneratorBuilder, tokenName: String): Session = {
-      def compileResource(json: String)(implicit s: Session): Option[String] =
-        ElCompiler.compile[String](json).apply(s).toOption
-
-      def generateJwt(implicit s: Session): Option[String] = {
-        compileResource(generator.header.json).flatMap(header =>
-          compileResource(generator.payload.json).map(payload =>
-            Jwt.encode(header, payload, generator.secret, generator.jwtAlgorithm)))
+      (for {
+        header  <- generator.header.json.el[String].apply(s)
+        payload <- generator.payload.json.el[String].apply(s)
+      } yield s.set(tokenName, Jwt.encode(header, payload, generator.secret, generator.jwtAlgorithm))) match {
+        case Success(session) => session
+        case Failure(message) => logger.error(s"Jwt generation failed: $message"); s
       }
-      s.set(tokenName, generateJwt(s).get)
     }
   }
 
