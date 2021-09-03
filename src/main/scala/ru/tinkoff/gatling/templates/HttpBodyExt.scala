2b24fcef0db1d03b50fe90c756fc3cda8d93954c
red-bashmak <bumshak@gmail.com>
2021-09-03 16:02:04 +0300

    fix: fix empty array encode to json as 'List()', added aliases arr and obj for construct corresponding types

--- src/main/scala/ru/tinkoff/gatling/templates/HttpBodyExt.scala
+++ src/main/scala/ru/tinkoff/gatling/templates/HttpBodyExt.scala
@@ -6,22 +6,21 @@ import io.gatling.http.request.builder.HttpRequestBuilder
 import ru.tinkoff.gatling.templates.Syntax._
 
 /**
- * This extension give ability to write something like this
- *
- * http("PostData")
- *    .post(url)
- *    .jsonBody(
- *      "id" - 23,                   // in json - "id" : 23
- *      "name",                      // in json it interpreted as - "name" : get value from session variable ${name}
- *      "project" - (                // in json - "project" : { ... }
- *        "id" ~ "projectId",        // in json - "id" : value from session var ${projectId}
- *        "name" - "Super Project",  // in json - "name": "Super Project"
- *        "sub" > ( 1,2,3,4,5,6)     // in json - "sub" : [ 1,2,3,4,5,6 ]
- *      )
- *    )
- *
-*/
-
+  * This extension give ability to write something like this
+  *
+  * http("PostData")
+  *    .post(url)
+  *    .jsonBody(
+  *      "id" - 23,                   // in json - "id" : 23
+  *      "name",                      // in json it interpreted as - "name" : get value from session variable ${name}
+  *      "project" - (                // in json - "project" : { ... }
+  *        "id" ~ "projectId",        // in json - "id" : value from session var ${projectId}
+  *        "name" - "Super Project",  // in json - "name": "Super Project"
+  *        "sub" > ( 1,2,3,4,5,6)     // in json - "sub" : [ 1,2,3,4,5,6 ]
+  *      )
+  *    )
+  *
+  */
 object HttpBodyExt {
   implicit class BodyOps(val httpRequestBuilder: HttpRequestBuilder) extends AnyVal {
     def body(string: String): HttpRequestBuilder = httpRequestBuilder.body(StringBody(string.el[String]))
@@ -36,7 +35,7 @@ object HttpBodyExt {
     def xmlBody(fs: Field*): HttpRequestBuilder =
       httpRequestBuilder
         .body(
-          StringBody(makeJson(fs: _*).el[String])
+          StringBody(makeXml(fs: _*).el[String])
         )
         .asXml
   }
