251ac6c2ba5888f2776fc21df54e70ba5d844d6e
g.matkovskiy <g.matkovskiy@tinkoff.ru>
2021-10-22 17:02:02 +0300

    feat: add functional style

--- src/main/scala/ru/tinkoff/gatling/feeders/VaultFeeder.scala
+++ src/main/scala/ru/tinkoff/gatling/feeders/VaultFeeder.scala
@@ -4,52 +4,46 @@ import io.gatling.core.feeder.Feeder
 import org.json4s.JValue
 import org.json4s.native.JsonMethods
 
-import java.io.OutputStream
-import java.net.{HttpURLConnection, URL}
-import scala.io.Source.fromInputStream
+import java.net.{URL, URLConnection}
+import scala.io.Source
+import scala.util.{Failure, Success, Using}
 
 object VaultFeeder {
+
+  private def getResponseFromConnection(connection: URLConnection): String = {
+    Using(Source.fromInputStream(connection.getInputStream)) { source =>
+      source.mkString
+    } match {
+      case Success(value) => value
+      case Failure(_)     => ""
+    }
+  }
 
   def apply(vaultUrl: String, secretPath: String, roleId: String, secretId: String, keys: List[String]): Feeder[String] = {
     var resultMap: Map[String, String] = Map[String, String]()
 
-    var url: URL               = new URL(vaultUrl + "/v1/auth/approle/login")
-    var con: HttpURLConnection = url.openConnection.asInstanceOf[HttpURLConnection]
-    con.setRequestMethod("POST")
-    con.setRequestProperty("Content-Type", "application/json")
-    con.setDoOutput(true)
+    val approleLoginConnection: URLConnection = new URL(vaultUrl + "/v1/auth/approle/login").openConnection
+    approleLoginConnection.setRequestProperty("Content-Type", "application/json")
+    approleLoginConnection.setDoOutput(true)
 
-    val body: String       = s"""{"role_id":"$roleId","secret_id":"$secretId"}"""
-    val os: OutputStream   = con.getOutputStream
-    val input: Array[Byte] = body.getBytes
-    os.write(input, 0, input.length)
+    val body: Array[Byte] = s"""{"role_id":"$roleId","secret_id":"$secretId"}""".getBytes
+    approleLoginConnection.getOutputStream.write(body, 0, body.length)
 
-    var response: String     = fromInputStream(con.getInputStream).mkString
-    var json: JValue         = JsonMethods.parse(response)
+    var json: JValue         = JsonMethods.parse(getResponseFromConnection(approleLoginConnection))
     val client_token: JValue = json \ "auth" \ "client_token"
     val vaultToken: String   = client_token.values.toString
 
-    url = new URL(vaultUrl + "/v1/" + secretPath)
-    con = url.openConnection.asInstanceOf[HttpURLConnection]
-    con.setRequestMethod("GET")
-    con.setRequestProperty("X-Vault-Token", vaultToken)
+    val vaultSecretsConnection: URLConnection = new URL(vaultUrl + "/v1/" + secretPath).openConnection
+    vaultSecretsConnection.setRequestProperty("X-Vault-Token", vaultToken)
 
-    response = fromInputStream(con.getInputStream).mkString
-    json = JsonMethods.parse(response)
+    json = JsonMethods.parse(getResponseFromConnection(vaultSecretsConnection))
     val data: JValue = json \ "data"
 
-    try {
-      val vaultMap: Map[String, Any] = data.values.asInstanceOf[Map[String, Any]]
+    data.foldField[Any](0)(
+      (_, kv) =>
+        if (keys.contains(kv._1))
+          resultMap += (kv._1 -> kv._2.values.toString))
 
-      for (k: String <- keys) {
-        if (vaultMap.contains(k)) {
-          resultMap += (k -> vaultMap(k).toString)
-        }
-      }
-      Iterator.continually(resultMap)
-    } catch {
-      case _: Exception =>
-    }
     Iterator.continually(resultMap)
   }
 
