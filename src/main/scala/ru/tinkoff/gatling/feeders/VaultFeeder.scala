4edfadfb0c450e051839e9f03941abb9b4881b8b
g.matkovskiy <g.matkovskiy@tinkoff.ru>
2021-10-26 12:09:38 +0300

    feat: add THttpClient

--- src/main/scala/ru/tinkoff/gatling/feeders/VaultFeeder.scala
+++ src/main/scala/ru/tinkoff/gatling/feeders/VaultFeeder.scala
@@ -3,58 +3,34 @@ package ru.tinkoff.gatling.feeders
 import io.gatling.core.feeder.{Feeder, Record}
 import org.json4s.native.JsonMethods
 import org.json4s.{DefaultFormats, JValue}
+import ru.tinkoff.gatling.utils.THttpClient
 
-import java.net.URI
-import java.net.http.HttpRequest.{BodyPublisher, BodyPublishers}
-import java.net.http.HttpResponse.BodyHandlers
-import java.net.http.{HttpClient, HttpRequest}
-import java.time.Duration
 import java.util.Objects.requireNonNull
-import scala.util.{Failure, Success, Try}
 
 object VaultFeeder {
 
-  private def getResponse(uri: String, header1: String, header2: String, method: String, body: BodyPublisher): String =
-    Try {
-      val client: HttpClient = HttpClient
-        .newBuilder()
-        .build()
-
-      val request: HttpRequest = HttpRequest.newBuilder
-        .uri(URI.create(uri))
-        .header(header1, header2)
-        .method(method, body)
-        .timeout(Duration.ofMinutes(1))
-        .build()
-
-      client.send(request, BodyHandlers.ofString).body
-    } match {
-      case Success(response) => response
-      case Failure(_)        => ""
-    }
-
-  def apply(vaultUrl: String, secretPath: String, roleId: String, secretId: String, keys: List[String]): Feeder[Any] = {
+  def apply(vaultUrl: String, secretPath: String, roleId: String, secretId: String, keys: List[String]): Feeder[String] = {
     requireNonNull(keys, "Keys list must not be null")
 
     implicit val formats: DefaultFormats = org.json4s.DefaultFormats
 
     val body: String = s"""{"role_id":"$roleId","secret_id":"$secretId"}"""
-    val vaultTokenResponse: String =
-      getResponse(s"""$vaultUrl/v1/auth/approle/login""",
-        "Content-Type",
-        "application/json",
-        "POST",
-        BodyPublishers.ofString(body))
+
+    val vaultTokenResponse: String = THttpClient()
+      .POSTJson(s"""$vaultUrl/v1/auth/approle/login""", body)
+      .body()
 
     val vaultTokenJson: JValue = JsonMethods.parse(vaultTokenResponse)
     val client_token: JValue   = vaultTokenJson \ "auth" \ "client_token"
     val vaultToken: String     = client_token.values.toString
 
-    val vaultDataResponse: String =
-      getResponse(s"""$vaultUrl/v1/$secretPath""", "X-Vault-Token", vaultToken, "GET", BodyPublishers.noBody)
+    val getHeaders: Seq[String] = Seq("X-Vault-Token", s"""$vaultToken""")
+    val vaultDataResponse: String = THttpClient()
+      .GET(s"""$vaultUrl/v1/$secretPath""", getHeaders)
+      .body()
 
     val vaultDataJson: JValue = JsonMethods.parse(vaultDataResponse)
-    val data: Record[Any]     = (vaultDataJson \ "data").extract[Map[String, Any]]
+    val data: Record[String]  = (vaultDataJson \ "data").extract[Map[String, String]]
 
     Iterator.continually(data.view.filterKeys(keys.contains).toMap)
   }
