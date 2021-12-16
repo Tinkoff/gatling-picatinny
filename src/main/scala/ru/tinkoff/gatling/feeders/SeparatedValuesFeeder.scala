afe58a7a136bada61377b2c384c5e67493c10a48
Ruslan Bay <r.bay@tinkoff.ru>
2021-12-16 22:29:32 +0300

    feat: update SeparatedValuesFeeder

--- src/main/scala/ru/tinkoff/gatling/feeders/SeparatedValuesFeeder.scala
+++ src/main/scala/ru/tinkoff/gatling/feeders/SeparatedValuesFeeder.scala
@@ -4,16 +4,20 @@ import io.gatling.core.Predef._
 import io.gatling.core.feeder.FeederBuilderBase
 
 object SeparatedValuesFeeder {
-
-  def apply(paramName: String, kv: Map[String, String], separator: Char): FeederBuilderBase[String] = {
-    kv.values.mkString
+  def apply(paramName: String, key: String, kv: Map[String, String], separator: Char): FeederBuilderBase[String] = {
+    kv.view
+      .filterKeys(key.contains)
+      .values
+      .mkString
       .split(separator)
       .toList
       .toFeeder(paramName)
   }
 
-  def fromCsv(paramName: String, kv: Map[String, String]): FeederBuilderBase[String] = apply(paramName, kv, ',')
-  def fromSsv(paramName: String, kv: Map[String, String]): FeederBuilderBase[String] = apply(paramName, kv, ';')
-  def fromTsv(paramName: String, kv: Map[String, String]): FeederBuilderBase[String] = apply(paramName, kv, '\t')
-
+  def fromCsv(paramName: String, key: String, kv: Map[String, String]): FeederBuilderBase[String] =
+    apply(paramName, key, kv, ',')
+  def fromSsv(paramName: String, key: String, kv: Map[String, String]): FeederBuilderBase[String] =
+    apply(paramName, key, kv, ';')
+  def fromTsv(paramName: String, key: String, kv: Map[String, String]): FeederBuilderBase[String] =
+    apply(paramName, key, kv, '\t')
 }
