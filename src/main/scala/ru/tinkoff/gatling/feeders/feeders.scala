f7fc42f7f3ed0c9a18a8b9149c8aaa591b70b992
i.akhaltsev <i.akhaltsev@tinkoff.ru>
2021-12-22 17:36:56 +0300

    feat: change feeders

--- src/main/scala/ru/tinkoff/gatling/feeders/feeders.scala
+++ src/main/scala/ru/tinkoff/gatling/feeders/feeders.scala
@@ -1,5 +1,6 @@
 package ru.tinkoff.gatling
 
+import io.gatling.core.config.GatlingConfiguration
 import io.gatling.core.feeder._
 
 package object feeders {
@@ -7,12 +8,15 @@ package object feeders {
   def feeder[T](name: String)(f: => T): Feeder[T] = Iterator.continually(Map(name -> f))
 
   implicit class FeederOps[A](val feeder: Feeder[A]) extends AnyVal {
-    def **[B](other: Feeder[B]): Feeder[Any]          = feeder.zip(other).map { case (r1, r2) => r1 ++ r2 }
-    def toFiniteLength(n: Int): IndexedSeq[Record[A]] = feeder.take(n).toIndexedSeq
+    def **[B](other: Feeder[B]): Feeder[Any] = feeder.zip(other).map { case (r1, r2) => r1 ++ r2 }
+
+    def toFiniteLength(n: Int)(implicit configuration: GatlingConfiguration): FeederBuilderBase[A] =
+      SourceFeederBuilder(InMemoryFeederSource(feeder.take(n).toIndexedSeq), configuration)
   }
 
-  implicit class Collection2FeederOps[A](val sequence: Iterable[A]) {
-    def toFeeder(name: String): IndexedSeq[Map[String, A]] = sequence.toIndexedSeq.map(x => Map(name -> x))
+  implicit class Collection2FeederOps[A](val sequence: Seq[A]) {
+    def toFeeder(name: String)(implicit configuration: GatlingConfiguration): FeederBuilderBase[A] =
+      SourceFeederBuilder(InMemoryFeederSource(sequence.map(x => Map(name -> x)).toIndexedSeq), configuration)
   }
 
 }
--- src/test/scala/ru/tinkoff/gatling/feeders/FeedersBaseSpec.scala
+++ src/test/scala/ru/tinkoff/gatling/feeders/FeedersBaseSpec.scala
@@ -1,5 +1,6 @@
 package ru.tinkoff.gatling.feeders
 
+import io.gatling.core.config.GatlingConfiguration
 import io.gatling.core.feeder._
 import org.scalacheck.Arbitrary._
 import org.scalacheck.Prop._
@@ -30,26 +31,35 @@ class FeedersBaseSpec extends AnyFlatSpec with Matchers {
     forAll { (n1: String, n2: String, v1: AnyVal, v2: AnyVal) =>
       val feeder1: Feeder[AnyVal] = Iterator.continually(Map(n1 -> v1))
       val feeder2: Feeder[AnyVal] = Iterator.continually(Map(n2 -> v2))
-      val result: Feeder[Any]     = feeder1 ** feeder2
+      val result: Feeder[Any] = feeder1 ** feeder2
       result.next().equals(Map(n1 -> v1, n2 -> v2))
     }.check()
   }
 
   it should "prepare feeder with finite size" in {
     forAll { (n: String, v: Char) =>
-      val fdr    = RandomDigitFeeder(n)
-      val result = fdr.toFiniteLength(v)
+      val fdr = RandomDigitFeeder(n)
+      val result = fdr.toFiniteLength(v)(GatlingConfiguration.loadForTest())
 
-      result.size == v
+      result.readRecords.size == v
+    }.check()
+  }
+
+  it should "transform Collection to Feeder" in {
+    forAll { (n: String, v: AnyVal) =>
+      val collection = Seq.fill(100)(v)
+      val result = collection.toFeeder(n)(GatlingConfiguration.loadForTest())
+
+      result.readRecords.forall(r => r.equals(Map(n -> v)))
     }.check()
   }
 
   it should "transform Collection to Feeder" in {
     forAll { (n: String, v: AnyVal) =>
       val collection = List.fill(100)(v)
-      val result     = collection.toFeeder(n)
+      val result = collection.toFeeder(n)(GatlingConfiguration.loadForTest())
 
-      result.forall(r => r.equals(Map(n -> v)))
+      result.readRecords.forall(r => r.equals(Map(n -> v)))
     }.check()
   }
 
