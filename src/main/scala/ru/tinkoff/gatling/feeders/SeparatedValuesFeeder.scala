c61386ddd7c6b35f3eb37c35bef2840a871d7540
Ruslan Bay <r.bay@tinkoff.ru>
2021-12-23 19:19:50 +0300

    feat: update SeparatedValuesFeeder

--- src/main/scala/ru/tinkoff/gatling/feeders/SeparatedValuesFeeder.scala
+++ src/main/scala/ru/tinkoff/gatling/feeders/SeparatedValuesFeeder.scala
@@ -1,81 +1,136 @@
 package ru.tinkoff.gatling.feeders
 
 import io.gatling.core.Predef._
+import io.gatling.core.feeder._
 import io.gatling.core.feeder.FeederBuilderBase
+import io.gatling.core.config.GatlingConfiguration
 
 object SeparatedValuesFeeder {
 
-  /** Creates a feeder with separated values from the source Map(k -> v) for the specified key
+  private val CommaSeparator: Char      = ','
+  private val SemicolonSeparator: Char  = ';'
+  private val TabulationSeparator: Char = '\t'
+
+  private def splitter(source: String, separator: Char): IndexedSeq[String] = source.split(separator).toIndexedSeq
+
+  /** Creates a feeder with separated values from the source String
     * @param paramName
     *   feeder name
-    * @param key
-    *   the key to get one single element from the source Map
     * @param source
     *   data source
     * @param separator
-    *   ",", ";", "\t" or other delimiter which separates values <br>you also can use following methods for the most common
-    *   separators: .fromCsv(...), .fromSsv(...), .fromTsv(...)
+    *   ',', ';', '\t' or other delimiter which separates values.
+    *
+    * You also can use following methods for the most common separators: .csv(...), .ssv(...), .tsv(...)
     * @return
     *   a new feeder
     * @example
     * {{{
-    *   val someFeeder = Iterator( Map( "k1" -> "v11,v12", "k2" -> "v21;v22;v23", "k3" -> "token" ) )
-    *   val sourceMap  = someFeeder.next()
+    *   val sourceString = "v21;v22;v23"
     *   val separatedValuesFeeder: FeederBuilderBase[String] =
-    *     SeparatedValuesFeeder("someValues", "k2", sourceMap, ";").random
+    *     SeparatedValuesFeeder("someValues", sourceString, ';') // this will return Vector(Map(someValues -> v21), Map(someValues -> v22), Map(someValues -> v23))
     * }}}
     */
-  def apply(paramName: String, key: String, source: Map[String, String], separator: String): FeederBuilderBase[String] = {
-    source.view
-      .filterKeys(key.contains)
-      .values
-      .mkString
-      .split(separator)
-      .toList
-      .toFeeder(paramName)
+  def apply(paramName: String, source: String, separator: Char)(implicit
+      configuration: GatlingConfiguration,
+  ): FeederBuilderBase[String] = {
+
+    val records = splitter(source, separator).map(s => Map(paramName -> s.trim))
+    require(records.nonEmpty, "Feeder source is empty")
+
+    SourceFeederBuilder(InMemoryFeederSource(records), configuration)
   }
 
-  def fromCsv(paramName: String, key: String, source: Map[String, String]): FeederBuilderBase[String] =
-    apply(paramName, key, source, ",")
-  def fromSsv(paramName: String, key: String, source: Map[String, String]): FeederBuilderBase[String] =
-    apply(paramName, key, source, ";")
-  def fromTsv(paramName: String, key: String, source: Map[String, String]): FeederBuilderBase[String] =
-    apply(paramName, key, source, "\t")
-
-  /** Creates a feeder with separated values from the source string
+  /** Creates a feeder with separated values from the source Sequence
     * @param paramName
     *   feeder name
     * @param source
-    *   source String or List
+    *   data source
     * @param separator
-    *   ",", ";", "\t" or other delimiter which separates values <br>you also can use following methods for the most common
-    *   separators: .fromCsv(...), .fromSsv(...), .fromTsv(...)
+    *   ',', ';', '\t' or other delimiter which separates values.
+    *
+    * You also can use following methods for the most common separators: .csv(...), .ssv(...), .tsv(...)
     * @return
     *   a new feeder
     * @example
     * {{{
-    *   val sourceString  = "v21;v22;v23"
+    *   val sourceSeq = Seq("1,two", "3,4")
     *   val separatedValuesFeeder: FeederBuilderBase[String] =
-    *     SeparatedValuesFeeder("someValues", sourceString, ";").random
+    *     SeparatedValuesFeeder.csv("someValues", sourceSeq) // this will return Vector(Map(someValues -> 1), Map(someValues -> two), Map(someValues -> 3), Map(someValues -> 4))
     * }}}
     */
-  def apply[T](paramName: String, source: T, separator: String): FeederBuilderBase[String] = source match {
-    case _: String         =>
-      s"$source"
-        .split(separator)
-        .toList
-        .toFeeder(paramName)
-    case source @ List(_*) =>
-      source
-        .mkString(separator)
-        .split(separator)
-        .toList
-        .toFeeder(paramName)
-    case _                 => throw new Exception(s"Source has to be String, List(Any) or Map(String -> String)")
+  def apply(paramName: String, source: Seq[String], separator: Char)(implicit
+      configuration: GatlingConfiguration,
+  ): FeederBuilderBase[String] = {
+
+    val records = source.flatMap(s => splitter(s, separator)).map(s => Map(paramName -> s.trim)).toIndexedSeq
+    require(records.nonEmpty, "Feeder source is empty")
+
+    SourceFeederBuilder(InMemoryFeederSource(records), configuration)
   }
 
-  def fromCsv[T](paramName: String, source: T): FeederBuilderBase[String] = apply(paramName, source, ",")
-  def fromSsv[T](paramName: String, source: T): FeederBuilderBase[String] = apply(paramName, source, ";")
-  def fromTsv[T](paramName: String, source: T): FeederBuilderBase[String] = apply(paramName, source, "\t")
+  /** Creates a feeder with separated values from the source Seq[Map[String, String] ]
+    * @param paramPrefix
+    *   feeder name
+    * @param source
+    *   data source
+    * @param separator
+    *   ',', ';', '\t' or other delimiter which separates values.
+    *
+    * You also can use following methods for the most common separators: .csv(...), .ssv(...), .tsv(...)
+    * @return
+    *   a new feeder
+    * @example
+    * {{{
+    *   val vaultFeeder: FeederBuilderBase[String] = Vector(
+    *     Map(
+    *       "HOSTS" -> "host11,host12",
+    *       "USERS" -> "user11",
+    *     ),
+    *     Map(
+    *       "HOSTS" -> "host21,host22",
+    *       "USERS" -> "user21,user22,user23",
+    *     ),
+    *   )
+    *   val mapFee: FeederBuilderBase[String] = SeparatedValuesFeeder(None, vaultFeeder.readRecords, ',')
+    *   val separatedValuesFeeder: FeederBuilderBase[String] =
+    *     SeparatedValuesFeeder("prefix", sourceSeq, ',') // this will return Vector(Map(HOSTS -> host11), Map(HOSTS -> host12), Map(USERS -> user11), Map(HOSTS -> host21), Map(HOSTS -> host22), Map(USERS -> user21), Map(USERS -> user22), Map(USERS -> user23))
+    * }}}
+    */
+  def apply(paramPrefix: Option[String], source: Seq[Map[String, Any]], separator: Char)(implicit
+      configuration: GatlingConfiguration,
+  ): FeederBuilderBase[String] = {
 
+    val records = source
+      .flatMap(m =>
+        m.map { case (k, v) =>
+          splitter(v.toString, separator).map {
+            paramPrefix match {
+              case None         => s => Map(k -> s)
+              case Some(prefix) => s => Map(s"${prefix}_$k" -> s)
+            }
+          }
+        },
+      )
+      .flatten
+      .toIndexedSeq
+    require(records.nonEmpty, "Feeder source is empty")
+
+    SourceFeederBuilder(InMemoryFeederSource(records), configuration)
+  }
+
+  def csv(paramName: String, source: String): FeederBuilderBase[String] = apply(paramName, source, CommaSeparator)
+  def ssv(paramName: String, source: String): FeederBuilderBase[String] = apply(paramName, source, SemicolonSeparator)
+  def tsv(paramName: String, source: String): FeederBuilderBase[String] = apply(paramName, source, TabulationSeparator)
+
+  def csv(paramName: String, source: Seq[String]): FeederBuilderBase[String] = apply(paramName, source, CommaSeparator)
+  def ssv(paramName: String, source: Seq[String]): FeederBuilderBase[String] = apply(paramName, source, SemicolonSeparator)
+  def tsv(paramName: String, source: Seq[String]): FeederBuilderBase[String] = apply(paramName, source, TabulationSeparator)
+
+  def csv(paramPrefix: Option[String] = None, source: Seq[Map[String, Any]]): FeederBuilderBase[String] =
+    apply(paramPrefix, source, CommaSeparator)
+  def ssv(paramPrefix: Option[String] = None, source: Seq[Map[String, Any]]): FeederBuilderBase[String] =
+    apply(paramPrefix, source, SemicolonSeparator)
+  def tsv(paramPrefix: Option[String] = None, source: Seq[Map[String, Any]]): FeederBuilderBase[String] =
+    apply(paramPrefix, source, TabulationSeparator)
 }
