de87e1b4afa8da4b789b584c6bf85b54b201f5d9
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-03-24 07:40:58 +0100

    Update jwt-core to 9.0.5 (#83)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -47,7 +47,7 @@ object Dependencies {
   )
 
   lazy val jwt: Seq[ModuleID] = Seq(
-    "com.github.jwt-scala" %% "jwt-core" % "9.0.4",
+    "com.github.jwt-scala" %% "jwt-core" % "9.0.5",
   )
 
   lazy val circeDeps: Seq[ModuleID] = Seq(
