a78c1dd580f4daaecf47ad8541b7be04e5f7cb6e
s.g.danilov <s.g.danilov@tinkoff.ru>
2022-10-27 14:56:43 +0300

    fix: RandomNatITNFeeder generates number which starts with 00

--- src/main/scala/ru/tinkoff/gatling/utils/RandomDataGenerators.scala
+++ src/main/scala/ru/tinkoff/gatling/utils/RandomDataGenerators.scala
@@ -61,12 +61,12 @@ object RandomDataGenerators {
 
   private def getRandomElement(items: List[Int], intLength: Int): Int = items match {
     case Nil => randomDigit(intLength)
-    case _ => items(randomDigit(items.length))
+    case _   => items(randomDigit(items.length))
   }
 
   private def getRandomElement(items: List[String], stringLength: Int): String = items match {
     case Nil => randomOnlyLettersString(stringLength)
-    case _ => items(randomDigit(items.length))
+    case _   => items(randomDigit(items.length))
   }
 
   def randomPAN(bins: String*): String = {
@@ -74,26 +74,26 @@ object RandomDataGenerators {
 
     def fifteenDigits(bins: List[String]): List[Char] = bins match {
       case Nil => s"""${digitString(6)}$idNum""".toList
-      case _ => s"""${getRandomElement(bins, 6)}$idNum""".toList
+      case _   => s"""${getRandomElement(bins, 6)}$idNum""".toList
     }
 
     val results: List[Int] = fifteenDigits(bins.toList).flatMap(_.toString.toIntOption)
-    val evenPosSum: Int = results.indices.collect {
+    val evenPosSum: Int    = results.indices.collect {
       case i if i % 2 == 0 => results(i)
     }.fold(0)((x, y) => x + (if (y * 2 > 9) y * 2 - 9 else y * 2))
-    val oddPosSum: Int = results.indices.collect { case i if i % 2 != 0 => results(i) }.sum
-    val controlNum: Int = 10 - (if ((oddPosSum + evenPosSum) % 10 == 0) 10 else (oddPosSum + evenPosSum) % 10)
+    val oddPosSum: Int     = results.indices.collect { case i if i % 2 != 0 => results(i) }.sum
+    val controlNum: Int    = 10 - (if ((oddPosSum + evenPosSum) % 10 == 0) 10 else (oddPosSum + evenPosSum) % 10)
 
     s"""${results.mkString("")}$controlNum"""
   }
 
   def randomOGRN(): String = {
-    val indicatorOGRN: Int = getRandomElement(List(1, 5), 1)
-    val year: String = String.format("%02d", randomDigit(2, 21))
+    val indicatorOGRN: Int   = getRandomElement(List(1, 5), 1)
+    val year: String         = String.format("%02d", randomDigit(2, 21))
     val ruSubjectNum: String = String.format("%02d", randomDigit(1, 90))
-    val idNum: String = digitString(7)
-    val result: String = s"""$indicatorOGRN$year$ruSubjectNum$idNum"""
-    val rem: Long = result.toLong % 11
+    val idNum: String        = digitString(7)
+    val result: String       = s"""$indicatorOGRN$year$ruSubjectNum$idNum"""
+    val rem: Long            = result.toLong % 11
 
     if (rem == 10)
       s"""${result}0"""
@@ -103,11 +103,11 @@ object RandomDataGenerators {
 
   def randomPSRNSP(): String = {
     val indicatorPSRNSP: Int = 3
-    val year: String = String.format("%02d", randomDigit(2, 21))
+    val year: String         = String.format("%02d", randomDigit(2, 21))
     val ruSubjectNum: String = String.format("%02d", randomDigit(1, 90))
-    val idNum: String = digitString(9)
-    val result: String = s"""$indicatorPSRNSP$year$ruSubjectNum$idNum"""
-    val rem: Long = result.toLong % 13 % 10
+    val idNum: String        = digitString(9)
+    val result: String       = s"""$indicatorPSRNSP$year$ruSubjectNum$idNum"""
+    val rem: Long            = result.toLong % 13 % 10
 
     if (rem == 10)
       s"""${result}0"""
@@ -117,8 +117,8 @@ object RandomDataGenerators {
 
   def randomKPP(): String = {
     val revenueServiceCode: String = String.format("%04d", randomDigit(1, 10000))
-    val reasonForReg: String = String.format("%02d", randomDigit(1, 100))
-    val idNum: String = String.format("%03d", randomDigit(1, 1000))
+    val reasonForReg: String       = String.format("%02d", randomDigit(1, 100))
+    val idNum: String              = String.format("%03d", randomDigit(1, 1000))
 
     s"""$revenueServiceCode$reasonForReg$idNum"""
   }
@@ -128,8 +128,8 @@ object RandomDataGenerators {
     @tailrec
     def itnNatRecursion(n: Int, sum: Int, results: List[Int]): String = {
       val rnd: Int = results.length match {
-        case 1 if results.head == 0 => randomDigit(1, 10)
-        case _ => randomDigit(0, 10)
+        case 1 if results.head == 0 => randomDigit(1,10)
+        case _ => randomDigit(0,10)
       }
 
       val factors: List[Int] = List(2, 4, 10, 3, 5, 9, 4, 6, 8)
@@ -149,8 +149,8 @@ object RandomDataGenerators {
 
     @tailrec
     def itnJurRecursion(n: Int, sum1: Int, sum2: Int, results: List[Int]): String = {
-      val rnd: Int = randomDigit(0, 10)
-      val firstFactors: List[Int] = List(7, 2, 4, 10, 3, 5, 9, 4, 6, 8)
+      val rnd: Int                 = randomDigit(0, 10)
+      val firstFactors: List[Int]  = List(7, 2, 4, 10, 3, 5, 9, 4, 6, 8)
       val secondFactors: List[Int] = List(3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8)
 
       def checkSum1: Int = sum1 + rnd * firstFactors(11 - n)
@@ -160,16 +160,16 @@ object RandomDataGenerators {
       n match {
         case 1 =>
           (results :+ (if ((sum2 + results.last * secondFactors(11 - n)) % 11 == 10) 0
-          else
-            (sum2 + results.last * secondFactors(11 - n)) % 11)).mkString("")
+                       else
+                         (sum2 + results.last * secondFactors(11 - n))   % 11)).mkString("")
         case 2 =>
           itnJurRecursion(
             n - 1,
             checkSum1,
             checkSum2,
             results :+ rnd :+ (if (checkSum1 % 11 == 10) 0
-            else
-              checkSum1 % 11),
+                               else
+                                 checkSum1   % 11),
           )
         case _ => itnJurRecursion(n - 1, checkSum1, checkSum2, results :+ rnd)
       }
@@ -189,11 +189,10 @@ object RandomDataGenerators {
       @tailrec
       def defineCheckSum(checkSum: Int): String = checkSum match {
         case x if x >= 10 && x < 100 => checkSum.toString
-        case x if x < 10 => s"0$checkSum"
-        case 100 | 101 => "00"
-        case _ => defineCheckSum(checkSum % 101)
+        case x if x < 10             => s"0$checkSum"
+        case 100 | 101               => "00"
+        case _                       => defineCheckSum(checkSum % 101)
       }
-
       n match {
         case 1 => (results :+ rnd :+ defineCheckSum(checkSum)).mkString("")
         case _ => snilsRecursion(n - 1, checkSum, results :+ rnd)
@@ -205,8 +204,8 @@ object RandomDataGenerators {
 
   def randomRusPassport(): String = {
     val ruSubjectNum: String = String.format("%02d", randomDigit(1, 90))
-    val year: String = String.format("%02d", randomDigit(0, 21))
-    val idNum: String = digitString(6)
+    val year: String         = String.format("%02d", randomDigit(0, 21))
+    val idNum: String        = digitString(6)
 
     s"""$ruSubjectNum$year$idNum"""
   }
@@ -218,13 +217,13 @@ object RandomDataGenerators {
     * YYYY-'W'ww-u 2001-W27-3
     */
   def randomDate(
-                  positiveDelta: Int,
-                  negativeDelta: Int,
-                  datePattern: String,
-                  dateFrom: LocalDateTime,
-                  unit: TemporalUnit,
-                  timezone: ZoneId,
-                ): String = {
+      positiveDelta: Int,
+      negativeDelta: Int,
+      datePattern: String,
+      dateFrom: LocalDateTime,
+      unit: TemporalUnit,
+      timezone: ZoneId,
+  ): String = {
     require(
       positiveDelta >= 0 && negativeDelta >= 0,
       s"RandomDateFeeder delta requires values >0. Current values: positiveDelta= $positiveDelta, negativeDelta= $negativeDelta",
@@ -236,12 +235,12 @@ object RandomDataGenerators {
   }
 
   def randomDate(
-                  offsetDate: Long,
-                  datePattern: String = "yyyy-MM-dd",
-                  dateFrom: LocalDateTime,
-                  unit: TemporalUnit,
-                  timezone: ZoneId,
-                ): String = {
+      offsetDate: Long,
+      datePattern: String = "yyyy-MM-dd",
+      dateFrom: LocalDateTime,
+      unit: TemporalUnit,
+      timezone: ZoneId,
+  ): String = {
     require(offsetDate > 1, s"RandomRangeDateFeeder offset requires value >1. Current values: offsetDate= $offsetDate")
     dateFrom.plus(randomDigit(1, offsetDate), unit).atZone(timezone).format(DateTimeFormatter.ofPattern(datePattern))
   }
