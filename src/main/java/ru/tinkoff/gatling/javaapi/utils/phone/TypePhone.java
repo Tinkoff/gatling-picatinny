80b374c77eaea7c85ab7108dbff912e164c180a0
k.sosnovskiy <k.sosnovskiy@tinkoff.ru>
2022-11-10 15:42:57 +0400

    chore: TypePhone as ADT type

---
+++ src/main/java/ru/tinkoff/gatling/javaapi/utils/phone/TypePhone.java
@@ -0,0 +1,15 @@
+package ru.tinkoff.gatling.javaapi.utils.phone;
+
+public final class TypePhone {
+    public static ru.tinkoff.gatling.utils.phone.TypePhone.TypePhone PhoneNumber(){
+        return ru.tinkoff.gatling.utils.phone.TypePhone.PhoneNumber$.MODULE$;
+    }
+
+    public static ru.tinkoff.gatling.utils.phone.TypePhone.TypePhone E164PhoneNumber(){
+        return ru.tinkoff.gatling.utils.phone.TypePhone.E164PhoneNumber$.MODULE$;
+    }
+
+    public static ru.tinkoff.gatling.utils.phone.TypePhone.TypePhone TollFreePhoneNumber(){
+        return ru.tinkoff.gatling.utils.phone.TypePhone.TollFreePhoneNumber$.MODULE$;
+    }
+}
