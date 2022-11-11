81af96694e29cbcef8940f6f21594ecd62ff2bb4
k.sosnovskiy <k.sosnovskiy@tinkoff.ru>
2022-11-11 12:03:54 +0400

    chore: add example Java and Kotlin

--- src/test/java/ru/tinkoff/gatling/javaapi/JavaFeedersTest.java
+++ src/test/java/ru/tinkoff/gatling/javaapi/JavaFeedersTest.java
@@ -10,6 +10,7 @@ import io.gatling.javaapi.core.Simulation;
 import ru.tinkoff.gatling.javaapi.utils.phone.PhoneFormatBuilder;
 import ru.tinkoff.gatling.utils.phone.PhoneFormat;
 
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
@@ -21,20 +22,21 @@ public class JavaFeedersTest extends Simulation {
     Iterator<Map<String, Object>> currentDateFeeder = CurrentDateFeeder("timeShort", DateTimeFormatter.ofPattern("MM:dd"));
     Iterator<Map<String, Object>> currentDateFeeder1 = CurrentDateFeeder("timeShort", DateTimeFormatter.ofPattern("MM:dd"), ZoneId.systemDefault());
 
-    Iterator<Map<String, Object>> customFeeder = CustomFeeder("custom", () -> "");
+    private String myFunction() {
+        byte[] array = new byte[7];
+        new Random().nextBytes(array);
+        return new String(array, StandardCharsets.UTF_8);
+    }
 
-    Iterator<Map<String, Object>> randomDateFeeder = RandomDateFeeder("randomDateFeeder", 5, "hh:mm:dd", LocalDateTime.of(2020, 1, 9, 9, 0), ChronoUnit.MINUTES, ZoneId.of("Australia/Sydney"));
-    Iterator<Map<String, Object>> randomDateFeeder1 = RandomDateFeeder("randomDateFeeder", "hh:mm:dd", LocalDateTime.of(2020, 1, 9, 9, 0), ChronoUnit.MINUTES, ZoneId.of("Australia/Sydney"));
-    Iterator<Map<String, Object>> randomDateFeeder2 = RandomDateFeeder("randomDateFeeder", LocalDateTime.of(2020, 1, 9, 9, 0), ChronoUnit.MINUTES, ZoneId.of("Australia/Sydney"));
-    Iterator<Map<String, Object>> randomDateFeeder3 = RandomDateFeeder("randomDateFeeder", ChronoUnit.MINUTES, ZoneId.of("Australia/Sydney"));
-    Iterator<Map<String, Object>> randomDateFeeder4 = RandomDateFeeder("randomDateFeeder", ZoneId.of("Australia/Sydney"));
-    Iterator<Map<String, Object>> randomDateFeeder5 = RandomDateFeeder("randomDateFeeder");
+    // custom feeder from provided function
+    Iterator<Map<String, Object>> customFeeder = CustomFeeder("myParam", this::myFunction);
+
+    Iterator<Map<String, Object>> randomDateFeeder = RandomDateFeeder("randomDateFeeder",1, 5, "hh:mm:dd", LocalDateTime.of(2020, 1, 9, 9, 0), ChronoUnit.MINUTES, ZoneId.of("Australia/Sydney"));
+    Iterator<Map<String, Object>> randomDateFeeder1 = RandomDateFeeder("randomDateFeeder",1, 5);
+
 
     Iterator<Map<String, Object>> randomDateRangeFeeder = RandomDateRangeFeeder("startOfVacation", "endOfVacation", 14L, "yyyy-MM-dd", LocalDateTime.now(), ChronoUnit.DAYS, ZoneId.of("Australia/Sydney"));
-    Iterator<Map<String, Object>> randomDateRangeFeeder1 = RandomDateRangeFeeder("startOfVacation", "endOfVacation", 14L, LocalDateTime.now(), ChronoUnit.DAYS, ZoneId.of("Australia/Sydney"));
-    Iterator<Map<String, Object>> randomDateRangeFeeder2 = RandomDateRangeFeeder("startOfVacation", "endOfVacation", 14L, ChronoUnit.DAYS, ZoneId.of("Australia/Sydney"));
-    Iterator<Map<String, Object>> randomDateRangeFeeder3 = RandomDateRangeFeeder("startOfVacation", "endOfVacation", 14L, ZoneId.of("Australia/Sydney"));
-    Iterator<Map<String, Object>> randomDateRangeFeeder4 = RandomDateRangeFeeder("startOfVacation", "endOfVacation", 14L);
+    Iterator<Map<String, Object>> randomDateRangeFeeder1 = RandomDateRangeFeeder("startOfVacation", "endOfVacation", 14L);
 
     Iterator<Map<String, Object>> randomDigitFeeder = RandomDigitFeeder("randomDigitFeeder");
     Iterator<Map<String, Object>> randomJurITNFeeder = RandomJurITNFeeder("randomJurITNFeeder");
@@ -54,9 +56,7 @@ public class JavaFeedersTest extends Simulation {
     Iterator<Map<String, Object>> randomPSRNSPFeeder = RandomPSRNSPFeeder("randomPSRNSPFeeder");
 
     Iterator<Map<String, Object>> randomRangeStringFeeder = RandomRangeStringFeeder("randomRangeStringFeeder");
-    Iterator<Map<String, Object>> randomRangeStringFeeder1 = RandomRangeStringFeeder("randomRangeStringFeeder", "abc");
-    Iterator<Map<String, Object>> randomRangeStringFeeder2 = RandomRangeStringFeeder("randomRangeStringFeeder", 1, "abc");
-    Iterator<Map<String, Object>> randomRangeStringFeeder3 = RandomRangeStringFeeder("randomRangeStringFeeder", 1, 5, "abc");
+    Iterator<Map<String, Object>> randomRangeStringFeeder1 = RandomRangeStringFeeder("randomRangeStringFeeder", 1, 5, "abc");
 
     Iterator<Map<String, Object>> randomRusPassportFeeder = RandomRusPassportFeeder("randomRusPassportFeeder");
     Iterator<Map<String, Object>> randomSNILSFeeder = RandomSNILSFeeder("randomSNILSFeeder");
@@ -82,8 +82,7 @@ public class JavaFeedersTest extends Simulation {
     Iterator<Map<String, Object>> separatedValuesFeeder11 = SeparatedValuesFeeder.tsv(Optional.of("asd"), source);
 
     Iterator<Map<String, Object>> sequentialFeeder = SequentialFeeder("sequentialFeeder");
-    Iterator<Map<String, Object>> sequentialFeeder1 = SequentialFeeder("sequentialFeeder", 0);
-    Iterator<Map<String, Object>> sequentialFeeder2 = SequentialFeeder("sequentialFeeder", 0, 1);
+    Iterator<Map<String, Object>> sequentialFeeder1 = SequentialFeeder("sequentialFeeder", 0, 1);
 
     private final String vaultUrl = System.getenv("vaultUrl");
     private final String secretPath = System.getenv("secretPath");
@@ -99,15 +98,8 @@ public class JavaFeedersTest extends Simulation {
                     .feed(customFeeder)
                     .feed(randomDateFeeder)
                     .feed(randomDateFeeder1)
-                    .feed(randomDateFeeder2)
-                    .feed(randomDateFeeder3)
-                    .feed(randomDateFeeder4)
-                    .feed(randomDateFeeder5)
                     .feed(randomDateRangeFeeder)
                     .feed(randomDateRangeFeeder1)
-                    .feed(randomDateRangeFeeder2)
-                    .feed(randomDateRangeFeeder3)
-                    .feed(randomDateRangeFeeder4)
                     .feed(randomDigitFeeder)
                     .feed(randomJurITNFeeder)
                     .feed(randomKPPFeeder)
@@ -122,8 +114,6 @@ public class JavaFeedersTest extends Simulation {
                     .feed(randomPSRNSPFeeder)
                     .feed(randomRangeStringFeeder)
                     .feed(randomRangeStringFeeder1)
-                    .feed(randomRangeStringFeeder2)
-                    .feed(randomRangeStringFeeder3)
                     .feed(randomRusPassportFeeder)
                     .feed(randomSNILSFeeder)
                     .feed(randomStringFeeder)
@@ -144,6 +134,5 @@ public class JavaFeedersTest extends Simulation {
                     .feed(separatedValuesFeeder11)
                     .feed(sequentialFeeder)
                     .feed(sequentialFeeder1)
-                    .feed(sequentialFeeder2)
                     .feed(vaultFeeder);
 }
