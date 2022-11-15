0bbc6b39ab2be77c0b41f56f2a8dda9e605023d5
Scala Steward <scala_steward@virtuslab.com>
2022-11-15 07:43:00 +0000

    Update jwt-core to 9.1.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -47,7 +47,7 @@ object Dependencies {
   )
 
   lazy val jwt: Seq[ModuleID] = Seq(
-    "com.github.jwt-scala" %% "jwt-core" % "9.1.1",
+    "com.github.jwt-scala" %% "jwt-core" % "9.1.2",
   )
 
   lazy val circeDeps: Seq[ModuleID] = Seq(
