e9acbba2d3e1fe9465af7804ee04a206822fdd5d
al.terekhov <al.terekhov@tinkoff.ru>
2022-11-30 10:34:58 +0300

    fix: el in userDataPoint

--- src/main/java/ru/tinkoff/gatling/javaapi/influxdb/Annotations.java
+++ src/main/java/ru/tinkoff/gatling/javaapi/influxdb/Annotations.java
@@ -1,22 +1,77 @@
 package ru.tinkoff.gatling.javaapi.influxdb;
 
 import io.gatling.javaapi.core.*;
-import io.razem.influxdbclient.Point;
+import io.razem.influxdbclient.*;
 
+import java.math.BigDecimal;
 import java.util.function.Function;
+import java.util.ArrayList;
+
+import static scala.jdk.CollectionConverters.IterableHasAsScala;
+import static io.gatling.javaapi.core.internal.Expressions.*;
 
 
 public final class Annotations {
+    public static Tag Tag(String key, String value) {
+        return Tag.apply(key, value);
+    }
+
+    public static Field Field(String key, String value) {
+        return StringField$.MODULE$.apply(key, value);
+    }
+
+    public static Field Field(String key, double value) {
+        return DoubleField$.MODULE$.apply(key, value);
+    }
+
+    public static Field Field(String key, long value) {
+        return LongField$.MODULE$.apply(key, value);
+    }
+
+    public static Field Field(String key, boolean value) {
+        return BooleanField$.MODULE$.apply(key, value);
+    }
+
+    public static Field Field(String key, BigDecimal value) {
+        return BigDecimalField$.MODULE$.apply(key, new scala.math.BigDecimal(value));
+    }
+
+    public static Point Point(String key) {
+        return new Point(
+                key,
+                System.currentTimeMillis() * 1000000,
+                IterableHasAsScala(new ArrayList<Tag>()).asScala().toSeq(),
+                IterableHasAsScala(new ArrayList<Field>()).asScala().toSeq()
+        );
+    }
+
+    public static Point Point(String key, long timestamp) {
+        return new Point(
+                key,
+                timestamp,
+                IterableHasAsScala(new ArrayList<Tag>()).asScala().toSeq(),
+                IterableHasAsScala(new ArrayList<Field>()).asScala().toSeq()
+        );
+    }
+
+    public static Point Point(String key, long timestamp, Iterable<Tag> tags, Iterable<Field> fields) {
+        return new io.razem.influxdbclient.Point(
+                key,
+                timestamp,
+                IterableHasAsScala(tags).asScala().toSeq(),
+                IterableHasAsScala(fields).asScala().toSeq()
+        );
+    }
 
     public static PopulationBuilder userDataPoint(String uniqScpName, Point point) {
         return new PopulationBuilder(ru.tinkoff.gatling.influxdb.Annotations.userDataPoint(uniqScpName, point));
     }
 
     public static PopulationBuilder userDataPoint(String uniqScpName,
-                                           String tagKey,
-                                           String tagValue,
-                                           String fieldKey,
-                                           String fieldValue) {
+                                                  String tagKey,
+                                                  String tagValue,
+                                                  String fieldKey,
+                                                  String fieldValue) {
         return new PopulationBuilder(
                 ru.tinkoff.gatling.influxdb.Annotations.userDataPoint(
                         uniqScpName,
@@ -35,15 +90,18 @@ public final class Annotations {
     }
 
     public static Function<Session, Session> userDataPoint(String tagKey,
-                                                            String tagValue,
-                                                            String fieldKey,
-                                                            String fieldValue){
+                                                           String tagValue,
+                                                           String fieldKey,
+                                                           String fieldValue) {
         return (Session session) -> {
-            String tag = tagValue;
-            String field = fieldValue;
-            if (session.contains(tag)) tag = session.getString(tag);
-            if (session.contains(field)) field = session.getString(field);
-            ru.tinkoff.gatling.influxdb.Annotations.influxDataPoint(tagKey, tag, fieldKey, field);
+            toStringExpression(tagValue).apply(session.asScala()).onSuccess((tag) -> {
+                toStringExpression(fieldValue).apply(session.asScala()).onSuccess((field) ->
+                {
+                    ru.tinkoff.gatling.influxdb.Annotations.influxDataPoint(tagKey, tag, fieldKey, field);
+                    return 0;
+                });
+                return 0;
+            });
             return session;
         };
     }
