6d5ee053b410b6e00d49847a82bdee3928c28529
Scala Steward <scala_steward@virtuslab.com>
2023-04-25 20:45:13 +0000

    Update jackson-dataformat-yaml to 2.15.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -32,7 +32,7 @@ object Dependencies {
 
   lazy val jackson: Seq[ModuleID] = Seq(
     "com.fasterxml.jackson.core"       % "jackson-core"            % "2.14.2",
-    "com.fasterxml.jackson.dataformat" % "jackson-dataformat-yaml" % "2.14.2",
+    "com.fasterxml.jackson.dataformat" % "jackson-dataformat-yaml" % "2.15.0",
   )
 
   lazy val scalaTest: Seq[ModuleID] = Seq(
